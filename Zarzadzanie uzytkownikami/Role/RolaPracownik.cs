c12ce0f30b5196798ff72f08a1b7e16402105314
Krzysiek <glodomorraa@gmail.com>
2011-12-28 14:16:03 +0100

    Poprawki

--- Zarzadzanie uzytkownikami/Role/RolaPracownik.cs
+++ Zarzadzanie uzytkownikami/Role/RolaPracownik.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Zarzadzanie_uzytkownikami.Role
+namespace Zarzadzanie_uzytkownikami
 {
     class RolaPracownik : Rola
     {
---
+++ Zarzadzanie uzytkownikami/Uzytkownik.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Zarzadzanie_uzytkownikami
+{
+    class Uzytkownik
+    {
+        String _imie;
+        String _nazwisko;
+        String _login;
+        String _haslo;
+
+        HashSet<Rola> _role = new HashSet<Rola>();
+
+        public String Imie
+        {
+            get { return _imie; }
+            set { _imie = value; }
+        }
+
+        public String Nazwisko
+        {
+            get { return _nazwisko; }
+            set { _nazwisko = value; }
+        }
+
+        public string Login
+        {
+            get { return _login; }
+            set { _login = value; }
+        }
+        public string Haslo
+        {
+            get { return _haslo; }
+            set { _haslo = value; }
+        }
+
+        public HashSet<Rola> Role{
+            get { return _role ; }
+            set { _role = value ; }
+        }
+                
+
+        public Uzytkownik(String login, String haslo, String imie, String nazwisko)
+        {
+            Login = login;
+            Haslo = haslo;
+            Imie = imie;
+            Nazwisko = nazwisko;
+        }
+
+
+        public void DodajRole(String nazwa)
+        {
+            if(nazwa=="klient")
+            {
+                Role.Add(new RolaKlient());         
+            }
+
+        }
+
+        public void UsunRole(Rola r)
+    }
+}
